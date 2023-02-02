506f4b1444b67b19d13606bedf0cc63dd9625364
Данила Тяпкин <dtapkin6@gmail.com>
2023-02-02 18:34:00 +0300

    Я ЭТО СДЕЛАЛ, Я ЭТО СДЕЛАЛ!!!

--- Частотный анализ/Program.cs
+++ Частотный анализ/Program.cs
@@ -2,29 +2,29 @@
 {
     private static void Main(string[] args)
     {
-        int k = 0, max = -1000;
+        int countVoice = 0, max = -1000, numToRemove = 0;
         // array filling 
+
         var counters = new int[17];
         // Data input
         for (int N = int.Parse(Console.ReadLine()); N > 0; N--)
         {
-            k = int.Parse(Console.ReadLine());
-            counters[k]++;
-            if (max < counters[k])
-                max = counters[k];
-            
+            countVoice = int.Parse(Console.ReadLine());
+            counters[countVoice]++;
+                if (max < counters[countVoice])
+                max = counters[countVoice];
         }
 
         // Pair counting and output
         for (int i = 0; i < counters.Length; i++)
         {
             if (counters[i] != 0 && max == counters[i])
-                Console.WriteLine($"{i} {counters[i]}");
-            if (i == counters.Length)
             {
-                max = counters[i]-1;
+                Console.WriteLine($"{i} {counters[i]}");
+                numToRemove = max;
+                counters = counters.Where(val => val != numToRemove).ToArray();
+                max--;
                 i = 0;
-                counters[i]=0;
             }
         }
     }
