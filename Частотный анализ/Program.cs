f72675787971910854c0c20e7aa5c83679b75946
Данила Тяпкин <dtapkin6@gmail.com>
2023-02-02 17:31:15 +0300

    ..

--- Частотный анализ/Program.cs
+++ Частотный анализ/Program.cs
@@ -1,19 +1,18 @@
 ﻿internal class Program
 {
-    const int maxArrayLength = 1000;
     private static void Main(string[] args)
     {
         // array filling 
-        var counters = new int[maxArrayLength];
+        var counters = new int[17];
         // Data input
-                for (int N = int.Parse(Console.ReadLine()); N > 0; N--)
-                    counters[int.Parse(Console.ReadLine())]++;
+        for (int N = int.Parse(Console.ReadLine()); N > 0; N--)
+            counters[int.Parse(Console.ReadLine())]++;
 
         // Pair counting and output
         for (int i = 0; i < counters.Length; i++)
         {
             if (counters[i] != 0)
-                Console.WriteLine($"{i} {counters[i]}");//
+                Console.WriteLine($"{i} {counters[i]}");
         }
     }
 }
