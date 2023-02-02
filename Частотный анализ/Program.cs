c35ead32dd8dfe1a5b939bef782f046b3507d13f
Данила Тяпкин <dtapkin6@gmail.com>
2023-02-02 18:09:06 +0300

    Пока что получается вывести только один максимум, либо несколько, которые равны по голосам...

--- Частотный анализ/Program.cs
+++ Частотный анализ/Program.cs
@@ -2,17 +2,30 @@
 {
     private static void Main(string[] args)
     {
+        int k = 0, max = -1000;
         // array filling 
         var counters = new int[17];
         // Data input
         for (int N = int.Parse(Console.ReadLine()); N > 0; N--)
-            counters[int.Parse(Console.ReadLine())]++;
+        {
+            k = int.Parse(Console.ReadLine());
+            counters[k]++;
+            if (max < counters[k])
+                max = counters[k];
+            
+        }
 
         // Pair counting and output
         for (int i = 0; i < counters.Length; i++)
         {
-            if (counters[i] != 0)
+            if (counters[i] != 0 && max == counters[i])
                 Console.WriteLine($"{i} {counters[i]}");
+            if (i == counters.Length)
+            {
+                max = counters[i]-1;
+                i = 0;
+                counters[i]=0;
+            }
         }
     }
 }
