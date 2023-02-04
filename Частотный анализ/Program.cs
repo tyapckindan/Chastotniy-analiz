73551aba16ca0b27f78789a715603b8ac7d67f44
Данила Тяпкин <dtapkin6@gmail.com>
2023-02-04 07:46:21 +0300

    Всё, мне лень в один проход делать.

--- Частотный анализ/Program.cs
+++ Частотный анализ/Program.cs
@@ -2,7 +2,7 @@
 {
     private static void Main(string[] args)
     {
-        int countVoice = 0, max = -1000, count = 0;
+        int countVoice = 0, max1 = 0, max2 = 0, max3 = 0, flag = 0;
         // array filling 
 
         var counters = new int[17];
@@ -11,27 +11,28 @@
         {
             countVoice = int.Parse(Console.ReadLine());
             counters[countVoice]++;
-            if (max == counters[countVoice])
-                count++;
-            if (max < counters[countVoice])
-                max = counters[countVoice];
         }
 
-        // Pair counting and output
-        for (int i = 0; i < counters.Length; i++)
-        {
-            if (counters[i] != 0 && max == counters[i])
+        //Search max
+        for (int i = 1; i < counters.Length; i++)
+            if (counters[i] > max1)
             {
-                {
-                    Console.WriteLine($"{i} {counters[i]}");
-                    count--;
-                }
-                if (count < 0)
-                {
-                    max--;
-                    i = 0;
-                }
+                max3 = max2;
+                max2 = max1;
+                max1 = counters[i];
             }
-        }
+
+        // Pair output
+        for (int i = 0; i < counters.Length; i++)
+            if (counters[i] != 0 && max1 == counters[i])
+                Console.WriteLine($"{i} {counters[i]}");
+
+        for (int i = 0; i < counters.Length; i++)
+            if (counters[i] != 0 && max2 == counters[i])
+                Console.WriteLine($"{i} {counters[i]}");
+
+        for (int i = 0; i < counters.Length; i++)
+            if (counters[i] != 0 && max3 == counters[i])
+                Console.WriteLine($"{i} {counters[i]}");    
     }
 }
